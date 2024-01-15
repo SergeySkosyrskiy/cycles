f1d939cfdf087bffa0832fd32b7b04830e44f61f
SergeySkosyrskiy <s-s-e_96@mail.ru>
2024-01-15 20:13:49 +0800

    Homework

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,56 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1");
 
-
-
-
-
-
-
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
+        System.out.println("Задача 2");
+        for (int i = 10; i >= 1; i = i - 1) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i = i - 1) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 8");
+        int savings = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + savings;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println("Задача 9");
+        int savings1 = 29000;
+        int total1 = 0;
+        for (int i = 1; i <= 12; i++) {
+            total1 = total1 + total1 / 100;
+            total1 = total1 + savings1;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total1 + " рублей");
+        }
+        System.out.println("Задача 10");
+
+        int j = 1;
+        for (int i = 2; j <= 10; j++) {
+            System.out.println(i + "*" + j + "=" + j);
+        }
+
     }
+}
